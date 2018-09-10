1c47eb4e25388fde832a0893d1f930f76e49d77a
damia <hasyadamia77@gmail.com>
2018-09-10 21:53:37 +0800

    Update Scrapper

--- stock-scrapper.py
+++ stock-scrapper.py
@@ -1,62 +1,113 @@
-from urllib.request import urlopen as uReq
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 import re
+import json
 
-# assuming review has 100 pages
-# number of review per page = 10
+# add to replace non ascii char
+# replace("/\u2013|\u2014/g", "-").replace("/\u2019s", "'")
 
-offset = 0
-total_page = 10
-number_per_page = 10
+base_url = "http://www.theedgemarkets.com"
 
-filename="review.csv"
-f = open(filename, "w", encoding="utf-8")
+years = [
+    "2018",
+    "2017",
+    "2016",
+    "2015",
+    "2014"
+]
 
-for p in range(0, total_page):
+companies = [
+    "maybank",
+    "axiata",
+    "cimb",
+    "petronas",
+    "sime darby"
+]
 
-    page = p + 1;
+def go_to_link(url):
+    search_url = url
+    req = Request(search_url, headers={'User-Agent': 'Mozilla/5.0'})
+    page_html = urlopen(req).read()
 
-    if page > 1:
-        offset = (number_per_page * page) - number_per_page
+    return soup(page_html,"html.parser")
 
-    myurl = "https://steamcommunity.com/app/431960/homecontent/?userreviewsoffset={}&p={}&numperpage={}".format(str(offset), str(page), str(number_per_page))
-    myurl += "&browsefilter=toprated&browsefilter=toprated&appid=431960&l=english&appHubSubSection=10&filterLanguage=english"
+for company in companies:
+    terminate = False
 
-    uClient = uReq(myurl)
-    page_html=uClient.read()
-    uClient.close()
+    company_dict = {}
+    index = 0
+    filename  = company.replace(" ", "-") + "-data.json"
+    file = open(filename, "w", encoding="utf-8")
 
-    page_soup = soup(page_html,"html.parser")
+    for page in range(0, 1000):
+        if terminate is True:
+            break
 
-    containers = page_soup.findAll("div",{"class":"apphub_UserReviewCardContent"})
-    
-    for container in containers:
-
-        # get date only from class=date_posted. remove unwanted word
-        date_posted = container.find('div', {'class': 'date_posted'})
-        date_posted = date_posted.text.replace('Posted: ', '').replace(',', '')
-        if not date_posted:
-            continue
-
-        # get recommended status inside class=reviewInfo
-        review_info_div = container.find('div', {'class': 'reviewInfo'})
-        status = review_info_div.find('div', {'class': 'title'})
-        status = status.text
-        if not status:
-            continue
-
-        # get review from class=apphub_CardTextContent
-        # remove class=date_posted and class=early_access_review that are inside class=apphub_CardTextContent
-        # we will get review data only. strip all whitespace
-        review = container.find('div', {'class': 'apphub_CardTextContent'})
-        review.find('div', {'class': 'date_posted'}).decompose()
-        review.find('div', {'class': 'early_access_review'}).decompose()
-        review = ' '.join(review.text.strip().split())
-        if not review:
-            continue
-
-        write_to_file = status + "|" + review + "|" + date_posted + "\n"
-        f.write(write_to_file)
-        print(write_to_file.encode("utf-8"))
+        url = base_url + "/search-results?page=" + str(page) + "&keywords=" + company + "+stock"
+        search_page = go_to_link(url)
+        rows = search_page.findAll("div",{"class":"views-row"})
         
-f.close()
+        for row in rows:
+
+            # get year, compared to years list
+            date_div = row.find('div', {'class': 'views-field views-field-nothing'})
+            date_posted = date_div.find('span', {'class': 'field-content'})
+
+            if not date_posted:
+                break
+
+            date_array = date_posted.text.split(" ")
+
+            del date_array[0]
+            date_value = "".join(date_array)
+
+            if not date_value:
+                break
+
+            year_posted = date_array[-1];
+
+            if year_posted not in years:
+                terminate = True
+                break
+
+            # get url
+            url_div = row.find('div', {'class': 'views-field views-field-title'})
+            url_div = url_div.find('span', {'class': 'field-content'})
+            article_url = url_div.find('a')['href']
+
+            # in article page
+            article_page = go_to_link(base_url + article_url)
+
+            # get article title
+            title_div = article_page.find('div', {'class': 'post-title'})
+            title = title_div.find('h1').text
+
+            if not title:
+                title = "No title"
+
+            # get article body
+            body_div = article_page.find('div', {'class': 'field field-name-body field-type-text-with-summary field-label-hidden'})
+            body_div = body_div.find('div', {'class': 'field-items'})
+            body_div = body_div.find('div', {'class': 'field-item even'})
+            body_p = body_div.findAll('p');
+
+            body_text = ""
+            for p in body_p:
+                body_text = body_text + " " + p.text
+
+            data_dict = {
+                'date_posted' : date_value,
+                'title' : title,
+                'article' : body_text
+            }
+
+            company_dict[index] = data_dict
+            index += 1
+            print(index)
+
+# write to file
+data_to_write = json.dumps(company_dict)
+file.write(data_to_write)
+print(data_to_write.encode("utf-8"))
+print("")
+file.close()
